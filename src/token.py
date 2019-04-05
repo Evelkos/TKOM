507058cbd0e34023106f26313048fac80389d24b
Ewelina <ewelkos.chmielewska@gmail.com>
2019-04-06 00:01:14 +0200

    new types, special characters and key words

--- src/token.py
+++ src/token.py
@@ -7,6 +7,42 @@ class Type(Enum):
     LIST = auto()
     NUMBER = auto()
     BOOL = auto()
+    ADD_OPERATOR = auto()
+    MULT_OPERATOR = auto()
+    OP_SQUARE_BRACKET = auto()
+    CL_SQUARE_BRACKET = auto()
+    OP_BRACKET = auto()
+    CL_BRACKET = auto()
+    OP_CURLY_BRACKET = auto()
+    CL_CURLY_BRACKET = auto()
+    SEMICOLON = auto()
+    DOT = auto()
+    COMMA = auto()
+    RETURN = auto()
+
+class Symbol:
+    special_characters = {
+        '[': Type.OP_SQUARE_BRACKET,
+        ']': Type.CL_SQUARE_BRACKET,
+        '(': Type.OP_BRACKET,
+        ')': Type.CL_BRACKET,
+        '{': Type.OP_CURLY_BRACKET,
+        '}': Type.CL_CURLY_BRACKET,
+        '*': Type.MULT_OPERATOR,
+        '/': Type.MULT_OPERATOR,
+        '+': Type.ADD_OPERATOR,
+        '-': Type.ADD_OPERATOR,
+        ';': Type.SEMICOLON,
+        '.': Type.DOT,
+        ',': Type.COMMA
+    }
+
+    key_words = {
+        'list': Type.LIST,
+        'number': Type.NUMBER,
+        'bool': Type.BOOL,
+        'return': Type.RETURN
+    }
 
 
 class Token:
